75f6659df9cd4cce803d9a246b8051a2dd12640f
Alexisdks <i202114481@cibertec.edu.pe>
2023-04-29 17:11:33 -0500

    Añadir info de Firebase equipo a nuestro proyecto

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { InfoPaginaService } from 'src/app/services/info-pagina.service';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
 })
 export class AboutComponent {
-
+  constructor(public _service: InfoPaginaService) {
+    
+  }
 }
--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { InfoPagina } from '../interfaces/info_pagina.interface';
+import { Equipo } from '../interfaces/equipo.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -8,15 +9,30 @@ import { InfoPagina } from '../interfaces/info_pagina.interface';
 export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
+  equipo: Equipo[] = [];
 
   constructor(private http: HttpClient) {
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo() {
     //Leer el archivo JSON
     this.http
       .get('assets/data/data_pagina.json')
       .subscribe((resp: InfoPagina) => {
         this.cargada = true;
         this.info = resp;
-        console.log(resp);
+      });
+  }
+
+  private cargarEquipo() {
+    this.http
+      .get<Equipo[]>(
+        'https://ng-temhtml-default-rtdb.firebaseio.com/equipo.json'
+      )
+      .subscribe((resp: Equipo[]) => {
+        this.equipo = resp;
       });
   }
 }
