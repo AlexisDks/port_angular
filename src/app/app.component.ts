a87b115a993fa84e55d518128d5fa225c775613c
Alexisdks <i202114481@cibertec.edu.pe>
2023-05-01 14:54:54 -0500

    Accediendo del firebase y mostrando producto ademas de el spinner cargar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { InfoPaginaService } from './services/info-pagina.service';
+import { ProductosService } from './services/productos.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  constructor(public _ingoPaginaService: InfoPaginaService) {
-    
-  }
+  constructor(
+    public _ingoPaginaService: InfoPaginaService,
+    public _productosService: ProductosService
+  ) {}
 }
---
+++ src/app/services/productos.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Producto } from '../interfaces/producto.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductosService {
+  cargando = true;
+  productos: Producto[] = [];
+
+  constructor(private http: HttpClient) {
+    this.cargarProductos();
+  }
+
+  private cargarProductos() {
+    this.http
+      .get<Producto[]>(
+        'https://ng-temhtml-default-rtdb.firebaseio.com/productos_idx.json'
+      )
+      .subscribe((resp: Producto[]) => {
+        this.productos = resp;
+
+        setTimeout(() => {
+          this.cargando = false;
+        }, 300);
+      });
+  }
+}
