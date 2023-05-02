36ff22c21a7bbd1306bba22dba2e114dafbc07af
Alexisdks <i202114481@cibertec.edu.pe>
2023-05-02 10:53:20 -0500

    Eliminando txt y log 2

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -15,8 +15,6 @@ export class SearchComponent {
 
   ngOnInit() {
     this.route.params.subscribe((params) => {
-      console.log(params['termino']);
-
       this.productoService.buscarProducto(params['termino']);
     });
   }
